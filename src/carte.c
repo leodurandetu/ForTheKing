b84eef2ec069f49059b92b5534e4a1dfb8272712
Durand Leo <Leo.Durand.Etu@univ-lemans.fr>
2026-01-29 11:48:09 +0100

    prototypes et structures de la carte à compléter

--- src/carte.c
+++ src/carte.c
@@ -5,9 +5,19 @@ void afficher_carte(case_t carte[TAILLE_CARTE][TAILLE_CARTE]) {
 
 }
 
+/* Leo */
+void init_carte(case_t carte[TAILLE_CARTE][TAILLE_CARTE]) {
+
+}
+
 /* Massoud */
 void generer_eau(case_t carte[TAILLE_CARTE][TAILLE_CARTE]) {
 
+}
+
+/* Massoud */
+case_t test_etat_case(case_t carte[TAILLE_CARTE][TAILLE_CARTE], int x, int y) {
+    
 }
 
 /* Saandi */
--- src/carte.h
+++ src/carte.h
@@ -5,9 +5,22 @@ typedef enum {
     DESERT
 } biome_t;
 
+typedef enum {
+    RIEN,
+    CAMPEMENT,
+    MAGASIN
+} type_bat_t;
+
+typedef struct {
+    
+    type_bat_t type;
+
+} batiment_t;
+
 typedef struct case_s {
 
     biome_t biome;
+    batiment_t batiment;
 
 } case_t;
 
@@ -17,5 +30,7 @@ case_t carte[TAILLE_CARTE][TAILLE_CARTE];
 
 /* Prototypes des fonctions */
 void afficher_carte(case_t carte[TAILLE_CARTE][TAILLE_CARTE]);
+void init_carte(case_t carte[TAILLE_CARTE][TAILLE_CARTE]);
 void generer_eau(case_t carte[TAILLE_CARTE][TAILLE_CARTE]);
 void generer_biomes(case_t carte[TAILLE_CARTE][TAILLE_CARTE]);
+case_t test_etat_case(case_t carte[TAILLE_CARTE][TAILLE_CARTE], int x, int y);
