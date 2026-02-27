b18ddcf9385b9bad0e460afd3e4db3b12f494839
TsmMassoud <ts.massoud@gmail.com>
2026-02-27 16:58:07 +0100

    Refactor carte.h with new structures and prototypes
    Updated structure definitions and added function prototypes for rendering and displaying the map.

--- lib/carte.h
+++ lib/carte.h
@@ -1,4 +1,12 @@
+#ifndef CARTE_H
+#define CARTE_H
+
 #include <SDL2/SDL.h>
+#include <math.h>
+
+/* Définitions */
+#define TAILLE_CARTE 50
+#define NB_COORD 4
 
 /* Structures */
 typedef enum {
@@ -17,37 +25,38 @@ typedef enum {
 } type_bat_t;
 
 typedef struct {
-    
     type_bat_t type;
-
 } batiment_t;
 
 typedef struct case_s {
-
     biome_t biome;
     batiment_t batiment;
     int estVisible;
-
 } case_t;
 
-typedef struct{
-    int x,y;
-}coordonnee_t;
+typedef struct {
+    int x, y;
+} coordonnee_t;
 
-#define TAILLE_CARTE 700
-#define NB_COORD 4
-
-/* Prototypes des fonctions */
-void afficher_carte(case_t carte[TAILLE_CARTE][TAILLE_CARTE]);
-void afficher_carte_sdl(SDL_Renderer * renderer,
-    case_t carte[TAILLE_CARTE][TAILLE_CARTE],
-    SDL_Texture * textures_cases[NB_BIOMES],
-    int taille_case, int persX, int persY,
-    int case_selection_x, int case_selection_y,
-    int perso_selectionne
-);
+/* Prototypes des fonctions de logique */
 void init_carte(case_t carte[TAILLE_CARTE][TAILLE_CARTE]);
 void generer_eau(case_t carte[TAILLE_CARTE][TAILLE_CARTE]);
 void generer_biomes(case_t carte[TAILLE_CARTE][TAILLE_CARTE]);
-int test_etat_case(case_t carte[TAILLE_CARTE][TAILLE_CARTE], int x, int y);
 void remplir_zone(case_t carte[TAILLE_CARTE][TAILLE_CARTE], biome_t biome, coordonnee_t dep, coordonnee_t arr);
+int test_etat_case(case_t carte[TAILLE_CARTE][TAILLE_CARTE], int x, int y);
+void afficher_carte(case_t carte[TAILLE_CARTE][TAILLE_CARTE]); // Console
+
+/* Prototypes des fonctions de rendu SDL2 */
+void afficher_hex_texture(SDL_Renderer* renderer, float cx, float cy, float rayon, SDL_Texture* texture, SDL_Color couleur);
+void dessiner_contour_double_dore(SDL_Renderer* r, float cx, float cy, float rayon, int alpha);
+void dessiner_contour_ftk(SDL_Renderer* r, float cx, float cy, float rayon, int epaisseur, SDL_Color couleur);
+void afficher_hex_couleur(SDL_Renderer* renderer, float cx, float cy, float rayon, SDL_Color couleur);
+void afficher_carte_sdl(SDL_Renderer * renderer,
+    case_t carte[TAILLE_CARTE][TAILLE_CARTE],
+    SDL_Texture * textures_cases[NB_BIOMES], 
+    int tailleCase,
+    int persX, int persY,
+    int case_selection_x, int case_selection_y,
+    int perso_selectionne);
+
+#endif 
