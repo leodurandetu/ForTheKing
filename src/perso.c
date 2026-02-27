ceb0e2e8cb9677c514be5b1ba1e32141735f33a8
TsmMassoud <ts.massoud@gmail.com>
2026-02-27 16:59:19 +0100

    Refactor character initialization and rendering

--- src/perso.c
+++ src/perso.c
@@ -7,8 +7,8 @@ perso_t * init_perso(perso_type_t persoType)
 {
     perso_t * perso = malloc(sizeof(perso_t));
 
-    perso->x = TAILLE_CARTE / 2;
-    perso->y = TAILLE_CARTE / 2;
+    perso->x = 25;
+    perso->y = 25;
 
     perso->type = persoType;
 
@@ -52,43 +52,44 @@ perso_t * init_perso(perso_type_t persoType)
     return perso;
 }
 
-void afficher_personnage(SDL_Renderer *renderer, SDL_Texture *texture, perso_t *pers, int tailleCase)
-{
-    int lFenetre, hFenetre;
-    SDL_GetRendererOutputSize(renderer, &lFenetre, &hFenetre);
-
-    int taillePerso = tailleCase / 2;
-    int marge = 2;
-    int epaisseurBord = 2;
-
-    SDL_Rect rectFond;
-    rectFond.w = taillePerso + (marge * 2);
-    rectFond.h = taillePerso + (marge * 2);
-    rectFond.x = (lFenetre / 2) - (rectFond.w / 2);
-    rectFond.y = (hFenetre / 2) - (rectFond.h / 2);
-
-    SDL_Rect rectBordure = {
-        rectFond.x - epaisseurBord,
-        rectFond.y - epaisseurBord,
-        rectFond.w + (epaisseurBord * 2),
-        rectFond.h + (epaisseurBord * 2)
-    };
-
-    /* Bordure Noire */
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderFillRect(renderer, &rectBordure);
-
-    /* Rectangle Blanc */
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderFillRect(renderer, &rectFond);
-
-    SDL_Rect dest;
-
-    dest.x = (lFenetre / 2) - (taillePerso / 2);
-    dest.y = (hFenetre / 2) - (taillePerso / 2);
+void afficher_personnage(SDL_Renderer * renderer, SDL_Texture * texture_perso, perso_t * perso, int tailleCase, int estSelectionne) {
     
-    dest.w = taillePerso;
-    dest.h = taillePerso;
+    int fenetre_taille_x, fenetre_taille_y;
+    SDL_GetRendererOutputSize(renderer, & fenetre_taille_x, & fenetre_taille_y);
 
-    SDL_RenderCopy(renderer, texture, NULL, &dest);
+    float rayon = tailleCase / 2.0f;
+    float hex_w = 2.0f * rayon;
+    float hex_h = sqrtf(3) * rayon;
+    float espacement_colonnes = hex_w * 0.75f;
+
+    float cx = perso -> x * espacement_colonnes + hex_w / 2.0f;
+    float cy = perso -> y * hex_h + (perso -> x % 2 ? hex_h / 2.0f : 0) + hex_h / 2.0f;
+
+    /* Caméra centrée sur le joueur (déduit du code main) */
+    float camX = cx - hex_w / 2.0f;
+    float camY = cy - hex_h / 2.0f;
+    int decalageX = (int)(fenetre_taille_x / 2 - camX - hex_w / 2.0f);
+    int decalageY = (int)(fenetre_taille_y / 2 - camY - hex_h / 2.0f);
+
+    float cx_finale = cx + decalageX;
+    float cy_finale = cy + decalageY;
+
+    /* Ajout du halo du selection */
+    if (estSelectionne) {
+        /* Couleur Cyan très claire, avec un alpha de 128 (moitié transparent) */
+        SDL_Color couleurHalo = { 100, 255, 255, 128 };
+        afficher_hex_couleur(renderer, cx_finale, cy_finale, rayon + 3.0f, couleurHalo);
+    }
+
+    /* Taill du personnage */
+    float echelle = 0.65f;
+
+    SDL_Rect dstRect;
+    dstRect.w = (int) (hex_w * echelle);
+    dstRect.h = (int) (hex_w * echelle); // Carré
+    /* Centrage automatique */
+    dstRect.x = (int)(cx_finale - dstRect.w / 2);
+    dstRect.y = (int)(cy_finale - dstRect.h / 2);
+
+    SDL_RenderCopy(renderer, texture_perso, NULL, & dstRect);
 }
