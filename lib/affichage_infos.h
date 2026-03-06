cfddd4b366987cbc5b50aeac6d6df62db8317a46
TsmMassoud <ts.massoud@gmail.com>
2026-03-06 11:36:05 +0100

    Add get_info_personnage function prototype

--- lib/affichage_infos.h
+++ lib/affichage_infos.h
@@ -3,8 +3,12 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
+#include "../lib/perso.h"
 
 /* Prototypes des fonctions */
 void mise_a_jour_texte(char * contenu, SDL_Renderer * renderer, TTF_Font* police, SDL_Surface ** texte, SDL_Texture ** texte_tex, SDL_Rect * txtDestRect);
+
+/* Massoud */
+void get_info_personnage(perso_t *perso, const char* attribut, char* buffer_sortie);
 
 #endif
