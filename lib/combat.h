3d4d024e6307f256ad261dc32529a7147d60181e
Leo Durand <Leo.Durand.Etu@univ-lemans.fr>
2026-03-11 09:46:57 +0100

    ajout de boutons sur la fenêtre de combat, ainsi qu'un fond blanc en dessous des personnages

--- lib/combat.h
+++ lib/combat.h
@@ -3,9 +3,11 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 #include "../lib/perso.h"
 #include "../lib/monstre.h"
 #include "../lib/combat.h"
+#include "math.h"
 
 typedef struct {
 
@@ -13,9 +15,18 @@ typedef struct {
     monstre_t * monstre;
     SDL_Window * pFenetre;
     SDL_Renderer * renderer;
+
     SDL_Texture * texture_monstre;
     SDL_Texture * texture_perso;
     SDL_Texture * texture_fond_ecran;
+
+    SDL_Rect bouton_leger;
+    SDL_Rect bouton_lourd;
+
+    int hover_bouton_leger;
+    int hover_bouton_lourd;
+
+    TTF_Font * font;
 
 } combat_t;
 
--- src/combat.c
+++ src/combat.c
@@ -1,5 +1,53 @@
 #include "../lib/combat.h"
 
+static void dessiner_bouton(combat_t *combat, SDL_Rect *r, int hover, const char *texte)
+{
+    SDL_Color fond = hover ? (SDL_Color){200,200,200,255} : (SDL_Color){150,150,150,255};
+
+    SDL_SetRenderDrawColor(combat->renderer, fond.r, fond.g, fond.b, 255);
+    SDL_RenderFillRect(combat->renderer, r);
+
+    SDL_SetRenderDrawColor(combat->renderer, 0, 0, 0, 255);
+    SDL_RenderDrawRect(combat->renderer, r);
+
+    if (!combat->font) return;
+
+    SDL_Color noir = {0, 0, 0, 255};
+
+    SDL_Surface *surf = TTF_RenderUTF8_Blended(combat->font, texte, noir);
+    SDL_Texture *tex = SDL_CreateTextureFromSurface(combat->renderer, surf);
+
+    SDL_Rect txt;
+    txt.w = surf->w;
+    txt.h = surf->h;
+    txt.x = r->x + (r->w - txt.w)/2;
+    txt.y = r->y + (r->h - txt.h)/2;
+
+    SDL_FreeSurface(surf);
+
+    SDL_RenderCopy(combat->renderer, tex, NULL, &txt);
+    SDL_DestroyTexture(tex);
+}
+
+static void dessiner_fond_ecran(combat_t *combat, int largeur, int hauteur)
+{
+    if (!combat->texture_fond_ecran) return;
+
+    int largeur_texture, hauteur_texture;
+    SDL_QueryTexture(combat->texture_fond_ecran, NULL, NULL, &largeur_texture, &hauteur_texture);
+
+    float echelle = fmax( (float) largeur / largeur_texture, (float) hauteur / hauteur_texture);
+
+    SDL_Rect rect;
+
+    rect.w = largeur_texture * echelle;
+    rect.h = hauteur_texture * echelle;
+    rect.x = (largeur - rect.w) / 2;
+    rect.y = (hauteur - rect.h) / 2;
+
+    SDL_RenderCopy(combat->renderer, combat->texture_fond_ecran, NULL, &rect);
+}
+
 void ouvrir_fenetre_combat(combat_t ** combat) {
     (*combat)->pFenetre = (SDL_CreateWindow("For The King - Combat", 
             SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
@@ -46,8 +94,13 @@ void ouvrir_fenetre_combat(combat_t ** combat) {
         SDL_FreeSurface(img_fond_foret);
     }
 
+    TTF_Init();
+    (*combat)->font = TTF_OpenFont("Fonts/Enchanted Land.otf", 24);
+
     int running = 1;
     int majAffichage = 1;
+
+    SDL_Point point;
 
     while (running) {
         SDL_Event e;
@@ -57,6 +110,28 @@ void ouvrir_fenetre_combat(combat_t ** combat) {
             switch (e.type) {
                 case SDL_QUIT:
                     running = 0;
+                    break;
+
+                case SDL_MOUSEMOTION:
+                    point.x = e.motion.x;
+                    point.y = e.motion.y;
+
+                    (*combat)->hover_bouton_leger = SDL_PointInRect(&point, &((*combat)->bouton_leger));
+                    (*combat)->hover_bouton_lourd = SDL_PointInRect(&point, &((*combat)->bouton_lourd));
+
+                    majAffichage = 1;
+                    break;
+
+                case SDL_MOUSEBUTTONDOWN:
+                    point.x = e.button.x;
+                    point.y = e.button.y;
+
+                    if (SDL_PointInRect(&point, &((*combat)->bouton_leger))) {
+                        printf("Attaque légère\n");
+                    } else if (SDL_PointInRect(&point, &((*combat)->bouton_lourd))) {
+                        printf("Attaque lourde\n");
+                    }
+
                     break;
 
                 case SDL_WINDOWEVENT:
@@ -86,56 +161,70 @@ void ouvrir_fenetre_combat(combat_t ** combat) {
     detruire_fenetre_combat(combat);
 }
 
-void maj_affichage_fenetre_combat(combat_t * combat) {
-    SDL_SetRenderDrawColor((combat->renderer), 255, 255, 255, 255);
-    SDL_RenderClear((combat->renderer));
+void maj_affichage_fenetre_combat(combat_t *combat)
+{
+    SDL_Renderer *renderer = combat->renderer;
 
-    int lFenetre, hFenetre;
-    SDL_GetRendererOutputSize((combat->renderer), &lFenetre, &hFenetre);
+    int largeur, hauteur;
+    SDL_GetRendererOutputSize(renderer, &largeur, &hauteur);
 
-    // Activation du mode BLEND pour que l'alpha fonctionne
-    SDL_SetRenderDrawBlendMode((combat->renderer), SDL_BLENDMODE_BLEND);
+    SDL_SetRenderDrawColor(renderer,255,255,255,255);
+    SDL_RenderClear(renderer);
 
-    if ((combat->texture_fond_ecran) != NULL) {
-        SDL_Rect rect;
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
 
-        rect.w = lFenetre;
-        rect.h = hFenetre;
-        rect.x = 0;
-        rect.y = (hFenetre / 2) - (rect.h / 2);
+    dessiner_fond_ecran(combat, largeur, hauteur);
 
-        SDL_RenderCopy((combat->renderer), (combat->texture_fond_ecran), NULL, &rect);
-    }
+    int taille = largeur * 0.18f;
+    int marge  = largeur * 0.03f;
 
-    if ((combat->texture_perso) != NULL) {
-        SDL_Rect rect;
+    SDL_Rect perso = {
+        marge,
+        hauteur / 2 - taille / 2,
+        taille,
+        taille
+    };
 
-        float echelle = 0.2f;
-        int taille = lFenetre * echelle;
+    SDL_Rect monstre = {
+        largeur - marge - taille,
+        hauteur / 2 - taille / 2,
+        taille,
+        taille
+    };
 
-        rect.w = taille;
-        rect.h = taille;
-        rect.x = 5;
-        rect.y = (hFenetre / 2) - (rect.h / 2);
+    /* fond blanc sous les personnages */
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(renderer, &perso);
+    SDL_RenderFillRect(renderer, &monstre);
 
-        SDL_RenderCopy((combat->renderer), (combat->texture_perso), NULL, &rect);
-    }
+    if (combat->texture_perso)
+        SDL_RenderCopy(renderer, combat->texture_perso, NULL, &perso);
 
-    if ((combat->texture_monstre) != NULL) {
-        SDL_Rect rect;
+    if (combat->texture_monstre)
+        SDL_RenderCopy(renderer, combat->texture_monstre, NULL, &monstre);
 
-        float echelle = 0.2f;
-        int taille = lFenetre * echelle;
+    /* boutons */
 
-        rect.w = taille;
-        rect.h = taille;
-        rect.x = lFenetre - 5 - rect.w;
-        rect.y = (hFenetre / 2) - (rect.h / 2);
+    int largeur_bouton = largeur * 0.22f;
+    int hauteur_bouton = hauteur * 0.09f;
+    int espace = largeur * 0.03f;
 
-        SDL_RenderCopy((combat->renderer), (combat->texture_monstre), NULL, &rect);
-    }
+    combat->bouton_leger = (SDL_Rect){
+        largeur / 2 - largeur_bouton - espace / 2,
+        hauteur - hauteur_bouton - marge,
+        largeur_bouton, hauteur_bouton
+    };
 
-    SDL_RenderPresent((combat->renderer));
+    combat->bouton_lourd = (SDL_Rect){
+        largeur / 2 + espace / 2,
+        hauteur - hauteur_bouton - marge,
+        largeur_bouton, hauteur_bouton
+    };
+
+    dessiner_bouton(combat, &combat->bouton_leger, combat->hover_bouton_leger, "Attaque Légère");
+    dessiner_bouton(combat, &combat->bouton_lourd, combat->hover_bouton_lourd, "Attaque Lourde");
+
+    SDL_RenderPresent(renderer);
 }
 
 void detruire_fenetre_combat(combat_t ** combat) {
