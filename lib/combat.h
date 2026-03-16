85471a43c36dba136c4a4a01ccc8d0adff39f980
TsmMassoud <ts.massoud@gmail.com>
2026-03-16 15:04:17 +0100

    Add new combat functions to combat.h

--- lib/combat.h
+++ lib/combat.h
@@ -53,6 +53,13 @@ typedef struct {
 extern void ouvrir_fenetre_combat(combat_t ** combat);
 extern void maj_affichage_fenetre_combat(combat_t * combat);
 extern void detruire_fenetre_combat(combat_t ** combat);
+/* Massoud */
 extern void dessiner_icone(combat_t *combat, SDL_Rect *r_icone, int survole, SDL_Texture *texture_arme, SDL_Texture *tex_texte, SDL_Rect *rect_texte);
 extern tour_t changer_tour(combat_t *combat);
+extern void attaque_lourde(combat_t *combat);
+extern void attaque_legere(combat_t *combat);
+extern void attaque_legere_monstre(combat_t *combat);
+extern void attaque_legere_monstre(combat_t *combat);
+extern void choix_attaque_monstre(combat_t *combat);
+
 #endif
