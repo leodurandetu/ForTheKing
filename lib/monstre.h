ac84439cacf76120f6b562e12b2f301a05c11d21
TsmMassoud <ts.massoud@gmail.com>
2026-03-16 21:28:04 +0100

    Update monstre.h

--- lib/monstre.h
+++ lib/monstre.h
@@ -2,9 +2,14 @@
 #define MONSTRE_H
 
 /* Structures */
+typedef enum{
+    SQUELETTE,
+    TROLL
+}type_monstre_t;
 
 typedef struct {
 
+    type_monstre_t type;
     int x, y;
     int degats;
     int force;
@@ -13,10 +18,11 @@ typedef struct {
     int intelligence;
     int rapidite;
     int evasion;
+    int niveau;
     
 } monstre_t;
 
 /* Prototypes des fonctions */
-monstre_t * creer_monstre_aleatoire(int x, int y);
+monstre_t * creer_monstre_aleatoire(type_monstre_t type,int x, int y);
 
 #endif
