abd916f35faa3f96da5fba53a63def3d3cf33403
Leo Durand <Leo.Durand.Etu@univ-lemans.fr>
2026-03-18 21:47:28 +0100

    il faut trouver un système de délai qui ne bloque pas le jeu, car si on clique plusieurs fois sur les boutons, le jeu va se geler pendant plusieurs secondes

--- src/combat.c
+++ src/combat.c
@@ -611,7 +611,6 @@ void attaque_legere(combat_t *combat){
     int pourcentage = 20 + rand() % 21; // 10% à 40%
     int vrai_degats = (pourcentage * combat->perso->degats)/100;
 
-    SDL_Delay(500); // 0;5 secondes
     combat->monstre->sante -= vrai_degats;
 
     if (combat->monstre->sante < 0){
@@ -623,7 +622,6 @@ void attaque_lourde(combat_t *combat){
     int pourcentage = 30 + rand() % 31; // 30% à 60%
     int vrai_degats = (pourcentage * combat->perso->degats)/100;
 
-    SDL_Delay(500); // 0;5 secondes
     combat->monstre->sante -= vrai_degats;
 
     if (combat->monstre->sante < 0){
@@ -635,7 +633,6 @@ void attaque_legere_monstre(combat_t *combat){
     int pourcentage = 5 + rand() % 26; // 10% à 40%
     int vrai_degats = (pourcentage * combat->monstre->degats)/100;
 
-    SDL_Delay(500); // 0;5 secondes
     combat->perso->sante -= vrai_degats;
 
     if(combat->perso->sante < 0){
@@ -647,7 +644,6 @@ void attaque_lourde_monstre(combat_t *combat){
     int pourcentage = 20 + rand() % 31; // 20% à 50%
     int vrai_degats = (pourcentage * combat->monstre->degats)/100;
 
-    SDL_Delay(500); // 0;5 secondes
     combat->perso->sante -= vrai_degats;
 
     if(combat->perso->sante < 0){
