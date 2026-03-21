0bdab14aed0c01460f1db48da73df8ee5d3ec4ee
TsmMassoud <ts.massoud@gmail.com>
2026-03-21 20:39:56 +0100

    Update niveau.c

--- src/niveau.c
+++ src/niveau.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include "../lib/niveau.h"
+#include "../lib/adapter_niveau.h"
 
 /* Leo
  * Cette fonction donne le nombre
@@ -6,13 +8,11 @@
  * passer au niveau suivant.
  */
 int xp_necessaire(int niveau) {
-
     if (niveau <= 1) {
         return 20;
     } else {
-        return (int) (1.5f * xp_necessaire(niveau - 1));
+        return (int)(1.5f * xp_necessaire(niveau - 1));
     }
-
 }
 
 /* Leo
@@ -20,14 +20,14 @@ int xp_necessaire(int niveau) {
  * le joueur a assez d'XP
  * pour monter d'un niveau
  */
-void verif_xp(perso_t * perso) {
+void verif_xp(perso_t *perso) {
     int xpMax = xp_necessaire(perso->niveau);
-
     if (perso->exp >= xpMax) {
         perso->niveau++;
         perso->exp -= xpMax;
+        appliquer_bonus_perso(perso);
+        printf("Niveau supérieur ! Niveau %d atteint.\n", perso->niveau);
     }
-
 }
 
 /* Leo
@@ -35,7 +35,7 @@ void verif_xp(perso_t * perso) {
  * points d'XP à un personnage
  * lorsqu'il a tué un monstre
  */
-void monstre_tue(perso_t * perso) {
+void monstre_tue(perso_t *perso) {
     perso->exp += 4;
     verif_xp(perso);
 }
