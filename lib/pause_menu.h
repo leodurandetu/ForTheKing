af267679b8ab16bb8b1563169270406322c35010
TsmMassoud <ts.massoud@gmail.com>
2026-03-27 18:55:46 +0100

    Update pause_menu.h

--- lib/pause_menu.h
+++ lib/pause_menu.h
@@ -4,11 +4,26 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 
-/* Massoud
- * Affiche le menu de pause par-dessus le jeu.
- * Retourne :
- *  0 = Continuer la partie
- *  1 = Quitter vers le menu
+/**
+ * @file pause_menu.h
+ * @author Massoud
+ * @brief Gestion de l'interface de pause en cours de partie.
+ */
+
+/* --- Prototypes des fonctions --- */
+
+/**
+ * @author Massoud
+ * @brief Affiche une interface de pause en superposition (overlay) du jeu.
+ * @details Cette fonction interrompt la logique principale pour capturer 
+ * les entrées utilisateur (Continuer ou Quitter).
+ * @param renderer Le moteur de rendu SDL2.
+ * @param police La police de caractères pour les options du menu.
+ * @param windowW Largeur de la fenêtre pour le centrage des éléments.
+ * @param windowH Hauteur de la fenêtre pour le centrage des éléments.
+ * @return int Code d'action utilisateur :
+ * - 0 : Reprendre la partie (Continuer).
+ * - 1 : Quitter la session actuelle et revenir au menu principal.
  */
 int afficher_menu_pause(SDL_Renderer* renderer, TTF_Font* police, int windowW, int windowH);
 
