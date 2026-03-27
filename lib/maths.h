d999d12a5b8fc919410aeca054389a1a60f56625
TsmMassoud <ts.massoud@gmail.com>
2026-03-27 18:54:34 +0100

    Update maths.h

--- lib/maths.h
+++ lib/maths.h
@@ -1,11 +1,36 @@
 #ifndef MATHS_H
 #define MATHS_H
 
-/* Constantes */
+/**
+ * @file maths.h
+ * @author Léo
+ * @brief Fonctions mathématiques utilitaires.
+ */
+
+/* --- Constantes --- */
+
+/**
+ * @author Léo
+ * @brief Valeur de la constante Pi.
+ */
 #define M_PI 3.14159265358979323846
 
-/* Prototype des fonctions */
+/* --- Prototypes des fonctions --- */
+
+/**
+ * @author Léo
+ * @brief Retourne la plus grande valeur entre deux entiers.
+ * @param a Premier entier.
+ * @param b Second entier.
+ */
 int max(int a, int b);
+
+/**
+ * @author Léo
+ * @brief Retourne la plus petite valeur entre deux entiers.
+ * @param a Premier entier.
+ * @param b Second entier.
+ */
 int min(int a, int b);
 
 #endif
