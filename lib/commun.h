945bb21c4afca9a61e37338540845b5a6b81cda9
TsmMassoud <ts.massoud@gmail.com>
2026-03-27 18:54:12 +0100

    Update commun.h

--- lib/commun.h
+++ lib/commun.h
@@ -1,9 +1,19 @@
 #ifndef COMMUN_H
 #define COMMUN_H
 
+/**
+ * @file commun.h
+ * @author Léo, Saandi & Massoud
+ * @brief Définitions de base partagées par l'ensemble des modules du projet.
+ */
+
+/**
+ * @author Léo, Saandi & Massoud
+ * @brief Type énuméré simulant un type booléen.
+ */
 typedef enum {
-    FAUX,
-    VRAI
+    FAUX, /**< Représente la valeur logique fausse (0). */
+    VRAI  /**< Représente la valeur logique vraie (1). */
 } booleen_t;
 
 #endif
