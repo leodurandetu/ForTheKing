6e2732be82d8fce65db0ef70bfbb1d18dac36e8c
TsmMassoud <ts.massoud@gmail.com>
2026-03-27 18:53:33 +0100

    Update carte_types.h

--- lib/carte_types.h
+++ lib/carte_types.h
@@ -1,12 +1,21 @@
 #ifndef CARTE_TYPES_H
 #define CARTE_TYPES_H
 
-/* Définitions */
+/**
+ * @file carte_types.h
+ * @author Léo, Saandi & Massoud
+ * @brief Définitions des structures et énumérations fondamentales de la carte.
+ */
+
+/* --- Définitions globales --- */
 #define TAILLE_CARTE 75
 #define NB_COORD 4
 #define INFINI 5000
 
-/* Structures */
+/** * @author Léo, Saandi & Massoud
+ * @brief Types de biomes disponibles sur la carte.
+ * @details Détermine la texture de fond de la case (sol).
+ */
 typedef enum {
     TERRE,
     EAU,
@@ -16,6 +25,9 @@ typedef enum {
     NB_BIOMES
 } biome_t;
 
+/** * @author Léo, Saandi & Massoud
+ * @brief Types d'obstacles ou éléments de décor superposés au biome.
+ */
 typedef enum {
     ARBRES,
     MONTAGNES,
@@ -25,6 +37,9 @@ typedef enum {
     NB_TERRAINS,
 } terrain_t;
 
+/** * @author Léo, Saandi & Massoud
+ * @brief Types de bâtiments interactifs présents sur la carte.
+ */
 typedef enum {
     PAS_DE_BATIMENT,
     CAMPEMENT,
@@ -33,8 +48,9 @@ typedef enum {
     TOMBE
 } type_bat_t;
 
-
-// Type des sanctuaires
+/** * @author Léo, Saandi & Massoud
+ * @brief Types de sanctuaires offrant des bonus aux statistiques.
+ */
 typedef enum {
     PAS_DE_SANCTUAIRE, 
     PUISSANCE,
@@ -43,23 +59,37 @@ typedef enum {
     EXPERIENCE
 } type_sanc_t;
 
+/** * @author Léo, Saandi & Massoud
+ * @brief Structure représentant un bâtiment.
+ */
 typedef struct {
-    type_bat_t type;
+    type_bat_t type; /**< Type spécifique du bâtiment. */
 } batiment_t;
 
+/** * @author Léo, Saandi & Massoud
+ * @brief Structure principale d'une cellule de la grille (la case).
+ * @details Regroupe toutes les informations d'occupation, d'environnement et d'affichage.
+ */
 typedef struct case_s {
-    type_sanc_t sanctuaires;
-    biome_t biome;
-    terrain_t terrain;
-    batiment_t batiment;
-    monstre_t * monstre;
-    int estVisible;
+    type_sanc_t sanctuaires; /**< Sanctuaire présent sur la case. */
+    biome_t biome;           /**< Type de biome (fond). */
+    terrain_t terrain;       /**< Type de terrain (obstacle). */
+    batiment_t batiment;     /**< Bâtiment présent sur la case. */
+    monstre_t * monstre;     /**< Pointeur vers un monstre présent, NULL si la case est vide de monstre. */
+    int estVisible;          /**< État pour le brouillard de guerre (1 pour visible, 0 pour caché). */
 } case_t;
 
+/** * @author Léo, Saandi & Massoud
+ * @brief Structure de positionnement dans un plan 2D.
+ */
 typedef struct {
-    int x, y;
+    int x; /**< Position horizontale. */
+    int y; /**< Position verticale. */
 } coordonnee_t;
 
+/** * @author Léo, Saandi & Massoud
+ * @brief Directions de déplacement possibles dans le jeu.
+ */
 typedef enum {
     HAUT,
     BAS,
